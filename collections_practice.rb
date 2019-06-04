104e7dbedefcc02e4ac607eae1ad9b71a6863d53
Kara O <karajo.odle@gmail.com>
2019-06-04 05:31:14 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -35,7 +35,7 @@ def count_elements(array)
   array.each do |hash|
     if last_hash == nil || hash[:name] != last_hash[:name]
       hash[:count] = 1
-      hash = last_hash
+      last_hash = hash
       new_arr << hash
     else
       last_hash[:count] += 1
